2110270a6589b3802208ec01e4b482c522d253a6
let-lucasOliveira <lucas.nro1000@gmail.com>
2025-07-21 18:47:58 -0300

    Add Pos, Put, Get and Update of Customer class.

---
+++ ProductClienteHub.api/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductClienteHub.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}")] // Fazendo desta forma impedimos o recebimento de ID nulo.
+        public IActionResult GetById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
